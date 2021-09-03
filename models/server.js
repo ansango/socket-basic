4a5a1395d3a09f779677c1dbba9d6129cffda1c1
ansango <anibalsantosgo@gmail.com>
2021-09-03 13:03:11 +0200

    cors

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
-
+const cors = require("cors");
 class Server {
   constructor() {
     this.app = express();
@@ -16,6 +16,7 @@ class Server {
 
   middleware() {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+    this.app.use(cors());
   }
 
   setupSockets() {
